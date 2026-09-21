ddea0b1c5a395b543e7693fb26ae4480a6f0e6a3
Anujsoni11 <anujsonisd2@gmail.com>
2026-09-21 16:09:24 +0530

    Completed create booking

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-const express=require('express');
-const app=express();
-const bodyParser=require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
 
-const {PORT}=require('./config/serverConfig');
-const apiRoutes=require('./routes/index');
-const db=require('./models/index');
+const { PORT } = require('./config/serverConfig');
+const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 
-const setupAndStartServer=()=>{
-    
+const setupAndStartServer = () => {
+
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({extended:true}));
+    app.use(bodyParser.urlencoded({ extended: true }));
 
-    app.use('/api',apiRoutes);
+    app.use('/api', apiRoutes);
 
 
-    app.listen(3002,()=>{
+    app.listen(3002, async () => {
         console.log(`Server Started on  PORT : ${PORT}`);
-        if(process.env.DB_SYNC){
-            db.sequelize.sync({alter:true});
+        if (process.env.DB_SYNC) {
+            db.sequelize.sync({ alter: true });
         }
     })
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 
 const { BookingController } = require('../../controllers/index');
-// const { createChannel } = require('../../utils/messageQueue');
-
-// const channel = await createChannel();
-const bookingController = new BookingController(channel);
 
 const router = express.Router();
 
-router.get('/info', (req, res) => {
-    return res.json({message: 'Response form routes'});
-})
-router.post('/booking', bookingController.create);
-router.post('/publish', bookingController.sendMessagesToQueue);
+router.post('/bookings', BookingController.create);
 
 module.exports = router;
